1395d3a97582317631b9a1a29d50bd310d0a1d7f
Morten <mtnielsen89@gmail.com>
2021-01-29 14:44:00 +0100

    Added update and delete to server functionality

--- store/actions/products.js
+++ store/actions/products.js
@@ -40,8 +40,17 @@ export const fetchProducts = () => {
   };
 };
 
-export const deleteProduct = (productId) => {
-  return { type: DELETE_PRODUCT, productId: productId };
+export const deleteProduct = (id) => {
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-complete-guide-shop-9252e-default-rtdb.europe-west1.firebasedatabase.app/products/${id}.json`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    dispatch({ type: DELETE_PRODUCT, productId: id });
+  };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -73,13 +82,26 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    },
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-complete-guide-shop-9252e-default-rtdb.europe-west1.firebasedatabase.app/products/${id}.json`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, description, imageUrl }),
+      }
+    );
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      },
+    });
   };
 };
