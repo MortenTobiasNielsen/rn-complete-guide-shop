d2636c0a73ade37e5f9e56ce2a486690625dbcce
Morten <mtnielsen89@gmail.com>
2021-01-30 15:09:42 +0100

    Added signup and login functionality

---
+++ store/actions/auth.js
@@ -0,0 +1,60 @@
+export const SIGNUP = "SIGNUP";
+export const LOGIN = "LOGIN";
+
+export const signup = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9jl8olkm5C2P3Efn6SnWKVF4mAzGcAaU",
+      {
+        method: "POST",
+        header: {
+          "Context-Type": "application.json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const resData = await response.json();
+
+    console.log(resData);
+
+    dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB9jl8olkm5C2P3Efn6SnWKVF4mAzGcAaU",
+      {
+        method: "POST",
+        header: {
+          "Context-Type": "application.json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const resData = await response.json();
+
+    console.log(resData);
+
+    dispatch({ type: LOGIN });
+  };
+};
